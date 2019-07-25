9cf10d9cf141dbfde4654cea286ce2656b43075b
Jack Collis <33093310+collis96@users.noreply.github.com>
2019-07-25 17:09:38 +0100

    complete layout and navigation implementation

--- app/src/main/java/com/arcticumi/convert/ActivityCurrency.java
+++ app/src/main/java/com/arcticumi/convert/ActivityCurrency.java
@@ -1,8 +1,16 @@
 package com.arcticumi.convert;
 
+
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,7 +21,32 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
 public class ActivityCurrency extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = "ActivityCurrency";
+
+    private static final String URL = "http://www.floatrates.com/daily/gbp.xml";
+    private TextView tvbCurrency;
+    private TextView tvtCurrency;
+    private TextView tvexRate;
+    private String strbCurrency;
+    private String strtCurrency;
+    private String strexRate;
+    private Button btnGo;
 
     private DrawerLayout drawer;
 
@@ -78,6 +111,104 @@ public class ActivityCurrency extends AppCompatActivity implements NavigationVie
                 R.string.navigation_drawer_open, R.string.navigation_drawer_closed);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        tvbCurrency = findViewById(R.id.tvBCurrency);
+        tvtCurrency = findViewById(R.id.tvTCurrency);
+        tvexRate = findViewById(R.id.tvExRate);
+
+        btnGo = findViewById(R.id.btnGo);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Starting AsyncTask");
+                DownloadData downloadData = new DownloadData();
+                downloadData.execute(URL);
+                Log.d(TAG, "onClick: Done.");
+            }
+        });
     }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        private static final String TAG = "DownloadData";
+
+        @Override
+        protected String doInBackground(String... urls) {
+            Log.d(TAG, "doInBackground: starts with " + urls[0]);
+            String rssFeed = downloadXML(urls[0]);
+            if (rssFeed == null) {
+                Log.e(TAG, "doInBackground: Error downloading");
+            }
+            return rssFeed;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(TAG, "onPostExecute: paramter is " + result);
+        }
+
+        private String downloadXML(String urlPath) {
+            StringBuilder xmlResult = new StringBuilder();
+
+            try {
+
+                URL url = new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                int response = connection.getResponseCode();
+                Log.d(TAG, "downloadXML: The response code was " + response);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                int charsRead;
+                char[] inputBuffer = new char[500];
+                while (true) {
+                    charsRead = reader.read(inputBuffer);
+                    if (charsRead < 0) {
+                        break;
+                    }
+                    if (charsRead > 0) {
+                        xmlResult.append(String.copyValueOf(inputBuffer, 0, charsRead));
+                    }
+                }
+                reader.close();
+                return xmlResult.toString();
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "downloadXML: Invalid URL" + e.getMessage());
+            } catch (IOException e) {
+                Log.e(TAG, "downloadXML: IO Exception reading data " + e.getMessage());
+            } catch (SecurityException e) {
+                Log.e(TAG, "downloadXML: Security Exception, needs permission?" + e.getMessage());
+            }
+
+            return null;
+        }
+    }
+
+//    private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+//        InputStream stream = null;
+//        // Instantiate the parser
+//        CurrencyXmlParser currencyXmlParser = new CurrencyXmlParser();
+//        List<CurrencyXmlParser.Item> items = null;
+//        String bCurrency = null;
+//        String tCurrency = null;
+//        String exRate = null;
+//
+//        try {
+//            stream = downloadUrl(urlString);
+//            items = currencyXmlParser.parse(stream);
+//            // Makes sure that the InputStream is closed after the app is
+//            // finished using it.
+//        } finally {
+//            if (stream != null) {
+//                stream.close();
+//            }
+//        }
+//
+//        for(CurrencyXmlParser.Item item : items){
+//            if(item.bCurrency=="GBP" && item.tCurrency=="EUR"){
+//                strbCurrency = item.bCurrency;
+//                strtCurrency = item.tCurrency;
+//                strexRate = item.exRate;
+//            }
+//        }
+//        return bCurrency;
+//    }
 
 }
--- app/src/main/java/com/arcticumi/convert/ActivityMain.java
+++ app/src/main/java/com/arcticumi/convert/ActivityMain.java
@@ -1,8 +1,12 @@
 package com.arcticumi.convert;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +14,21 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 
-public class ActivityMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ActivityMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
-    private static final String TAG = "ActivityMain";
     private DrawerLayout drawer;
+    private ImageView ivDistance;
+    private ImageView ivArea;
+    private ImageView ivVolume;
+    private ImageView ivTime;
+    private ImageView ivCurrency;
+    private ImageView ivTemperature;
+    private ImageView ivWeight;
+    private ImageView ivStorage;
+    private ImageView ivMagnetism;
+    private ImageView ivRadiation;
+    private ImageView ivFluid;
+    private ImageView ivOther;
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -61,6 +76,48 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ivDistance:
+                startActivity(new Intent(this, ActivityDistance.class));
+                break;
+            case R.id.ivArea:
+                startActivity(new Intent(this, ActivityArea.class));
+                break;
+            case R.id.ivVolume:
+                startActivity(new Intent(this, ActivityVolume.class));
+                break;
+            case R.id.ivTime:
+                startActivity(new Intent(this, ActivityTime.class));
+                break;
+            case R.id.ivCurrency:
+                startActivity(new Intent(this, ActivityCurrency.class));
+                break;
+            case R.id.ivTemp:
+                startActivity(new Intent(this, ActivityTemp.class));
+                break;
+            case R.id.ivWeight:
+                startActivity(new Intent(this, ActivityWeight.class));
+                break;
+            case R.id.ivStorage:
+                startActivity(new Intent(this, ActivityStorage.class));
+                break;
+            case R.id.ivMagnetism:
+                startActivity(new Intent(this, ActivityMagnetism.class));
+                break;
+            case R.id.ivRadiation:
+                startActivity(new Intent(this, ActivityRadiation.class));
+                break;
+            case R.id.ivFluid:
+                startActivity(new Intent(this, ActivityFluid.class));
+                break;
+            case R.id.ivOther:
+                startActivity(new Intent(this, ActivityOther.class));
+                break;
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -77,6 +134,31 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        ivDistance = findViewById(R.id.ivDistance);
+        ivArea = findViewById(R.id.ivArea);
+        ivVolume = findViewById(R.id.ivVolume);
+        ivTime = findViewById(R.id.ivTime);
+        ivCurrency = findViewById(R.id.ivCurrency);
+        ivTemperature = findViewById(R.id.ivTemp);
+        ivWeight = findViewById(R.id.ivWeight);
+        ivStorage = findViewById(R.id.ivStorage);
+        ivMagnetism = findViewById(R.id.ivMagnetism);
+        ivRadiation = findViewById(R.id.ivRadiation);
+        ivFluid = findViewById(R.id.ivFluid);
+        ivOther = findViewById(R.id.ivOther);
+
+        ivDistance.setOnClickListener(this);
+        ivArea.setOnClickListener(this);
+        ivVolume.setOnClickListener(this);
+        ivTime.setOnClickListener(this);
+        ivCurrency.setOnClickListener(this);
+        ivTemperature.setOnClickListener(this);
+        ivWeight.setOnClickListener(this);
+        ivStorage.setOnClickListener(this);
+        ivMagnetism.setOnClickListener(this);
+        ivRadiation.setOnClickListener(this);
+        ivFluid.setOnClickListener(this);
+        ivOther.setOnClickListener(this);
 
     }
 
